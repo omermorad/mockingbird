66ed75f8e05467db1adbdc93a144268791a9cb1e
Omer Morad <omer.moradd@gmail.com>
2021-07-11 19:29:46 +0300

    chore: fix typo in test name

--- src/factories/mock-factory.test.ts
+++ src/factories/mock-factory.test.ts
@@ -8,7 +8,7 @@ jest.mock('../class-processor', () => ({
   }),
 }));
 
-describe('Mock Factory - Unit', () => {
+describe('MockFactory - Unit', () => {
   describe('given a Mock Factory', () => {
     afterEach(() => {
       processMock.mockClear();
@@ -17,18 +17,12 @@ describe('Mock Factory - Unit', () => {
     class TestClass {}
 
     describe("when calling 'create' method without options", () => {
-      // let instance: TestClass;
-
       test('then call process exactly once', () => {
         MockFactory.create(TestClass);
 
         expect(processMock).toHaveBeenCalledTimes(1);
         expect(processMock).toHaveBeenCalledWith(TestClass);
       });
-
-      // test('then return an instance of the target class', () => {
-      //   expect(instance).toBeInstanceOf(TestClass);
-      // });
     });
 
     describe("when calling 'create' method with count = 3", () => {
