dca26558e7476f17a5e100204764177ad8ce74b1
Omer Morad <omer.moradd@gmail.com>
2021-07-13 18:21:43 +0300

    refactor: temp change to another class type

--- src/types/mock-options.type.ts
+++ src/types/mock-options.type.ts
@@ -10,10 +10,14 @@ export type EnumObject = { enum: Record<string, unknown> };
 
 export type Callback = (faker: Faker.FakerStatic) => any;
 
-export interface Class<T = any> extends Function {
-  new (...args: any[]): T;
-}
+// export interface Class<T = any> extends Function {
+//   new (...args: any[]): T;
+// }
+
+export type Class<T = any> = new (...arg: any[]) => T;
 
 export type ClassLiteral<TClass extends any = any> = { [K in keyof TClass]: TClass[K] };
 
 export type MockOptions = Callback | ExactValue | Class | EnumObject | MultiClass;
+
+export type GeneratedMock<TClass extends Class = any> = Class<TClass> | ClassLiteral<TClass>;
