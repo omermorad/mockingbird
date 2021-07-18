aeb13828aadc611bee94801235a292ffdbda4988
omermorad <omer.moradd@gmail.com>
2021-07-18 15:03:37 +0300

    refactor(types): change 'class' to 'type' with new definition

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -8,22 +8,20 @@ export interface ObjectLiteral {
   [key: string]: any;
 }
 
+export interface Type<T = any> extends Function {
+  new (...args: any[]): T;
+}
+
 export type ExactValue = string | number | boolean | ObjectLiteral | Date;
 
-export type MultiClass = { type: Class; count: number };
+export type MultiClass = { type: Type; count: number };
 
 export type EnumObject = { enum: Record<string, unknown> };
 
 export type Callback = (faker: FakerStatic) => any;
 
-// export interface Class<T = any> extends Function {
-//   new (...args: any[]): T;
-// }
-
-export type Class<T = any> = new (...arg: any[]) => T;
-
 export type ClassLiteral<TClass extends any = any> = { [K in keyof TClass]: TClass[K] };
 
-export type MockOptions = Callback | ExactValue | Class | EnumObject | MultiClass;
+export type MockOptions = Callback | ExactValue | Type | EnumObject | MultiClass;
 
-export type GeneratedMock<TClass extends Class = any> = Class<TClass> | ClassLiteral<TClass>;
+export type GeneratedMock<TClass extends Type = any> = Type<TClass> | ClassLiteral<TClass>;
