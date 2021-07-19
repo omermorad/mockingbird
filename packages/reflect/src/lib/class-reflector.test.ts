656661fb241190681a9f262d36d2b09d4965a754
Omer Morad <omer.moradd@gmail.com>
2021-07-19 13:38:18 +0300

    test: add some more test cases (and increase code coverage) (#57)
    * test(parser): check and spy on faker functions
    
    * test(parser): improve array-value-handler test cases
    
    * test(reflect): add missing test case (branch)
    
    * test(reflect): add missing test for property decorator value
    
    * chore(parser): change method name

--- packages/reflect/src/lib/class-reflector.test.ts
+++ packages/reflect/src/lib/class-reflector.test.ts
@@ -1,5 +1,5 @@
 import { ClassReflector } from './class-reflector';
-import { Mock } from '../decorators/mock.decorator';
+import { Mock } from '../decorators';
 import { ClassReflectionDto } from '../types/class-reflection-dto.type';
 
 describe('ClassReflector', () => {
@@ -21,13 +21,13 @@ describe('ClassReflector', () => {
     });
 
     describe("when calling 'reflectClass'", () => {
-      describe('and there are no related decorators on any of the properties', () => {
-        test('then return empty array of properties', () => {
+      describe('and there are no mock decorators on any of the properties', () => {
+        test('then return an empty array of properties', () => {
           expect(reflector.reflectClass(EmptyClass)).toHaveLength(0);
         });
       });
 
-      describe('and there are some related decorators in the class', () => {
+      describe('and the some of the properties in the class are decorated with mock decorator', () => {
         let classReflection: ClassReflectionDto;
 
         beforeAll(() => {
@@ -40,11 +40,23 @@ describe('ClassReflector', () => {
         });
 
         test('then create a property dto for each of the properties', () => {
-          expect(Object.keys(classReflection[0])).toEqual(['name', 'constructorName', 'decoratorValue']);
+          const reflectedClassKeys = Object.keys(classReflection[0]);
+          expect(reflectedClassKeys).toEqual(['name', 'constructorName', 'decoratorValue']);
         });
 
         test('then register the class in the reflected classes storage', () => {
           expect(ClassReflector.REFLECTED_CLASSES).toHaveProperty(TestClass.name);
+        });
+
+        describe('and the class has been already reflected once before', () => {
+          test('then return the dto of the first class with the name that has been parsed', () => {
+            class TestClass {
+              @Mock() someNewProperty: string;
+            }
+
+            const result = reflector.reflectClass(TestClass);
+            expect(result).not.toHaveProperty('someNewProperty');
+          });
         });
       });
     });
