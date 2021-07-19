2e65fd78124c0e0cf06f51b4c3dc64cf574e1a19
omermorad <omer.moradd@gmail.com>
2021-07-19 21:15:01 +0300

    refactor(types): remove type 'type' and add 'class' type instead

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -7,21 +7,14 @@ export const Faker = faker;
 export interface ObjectLiteral {
   [key: string]: any;
 }
-
-export interface Type<T = any> extends Function {
-  new (...args: any[]): T;
-}
+export type Class<T = any> = new (...args: any[]) => T;
 
 export type ExactValue = string | number | boolean | ObjectLiteral | Date;
 
-export type MultiClass = { type: Type; count: number };
+export type MultiClass = { type: Class; count: number };
 
 export type EnumObject = { enum: Record<string, unknown> };
 
 export type Callback = (faker: FakerStatic) => any;
 
-export type ClassLiteral<TClass extends any = any> = { [K in keyof TClass]: TClass[K] };
-
-export type MockOptions = Callback | ExactValue | Type | EnumObject | MultiClass;
-
-export type GeneratedMock<TClass extends Type = any> = Type<TClass> | ClassLiteral<TClass>;
+export type ClassLiteral<TClass = any> = { [K in keyof TClass]: TClass[K] };
