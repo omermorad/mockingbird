f4e3092e683eb9c288d4e879113e71f74ec5038a
Omer Morad <omer.moradd@gmail.com>
2021-07-23 08:34:24 +0300

    fix(types): add build to export a js file to include the faker instance (for runtime) (#61)
    * build(repo): fix tsconfig settings and build strategy
    
    * fix(types): add build to export a js file to include the faker instance (for runtime)
    
    * chore(repo): add coverage settings to jest config files and exclude some files from tsconfig

--- packages/generator/jest.config.js
+++ packages/generator/jest.config.js
@@ -6,4 +6,6 @@ module.exports = {
   roots: [...base.roots, '<rootDir>/test'],
   name: packageJson.name,
   displayName: packageJson.name,
+  collectCoverageFrom: ['src/**/*.ts', 'test/**/*.test.js'],
+  coveragePathIgnorePatterns: ['index.ts', 'test-classes.ts'],
 };
