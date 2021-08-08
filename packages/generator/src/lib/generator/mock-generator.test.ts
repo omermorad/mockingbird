64f0b20bb7be96d08066b4215adfd63e27517aa3
omermorad <omer.moradd@gmail.com>
2021-08-08 21:19:34 +0300

    test(mockingbird-ts): refactor generator test

--- packages/generator/src/lib/generator/mock-generator.test.ts
+++ packages/generator/src/lib/generator/mock-generator.test.ts
@@ -1,62 +1,96 @@
-import { ClassParser } from '@mockinbird/parser';
 import { MockGenerator } from './mock-generator';
-import { MockGeneratorOptions } from '../../types/mock-generator-options.interface';
+import { ClassParser } from '@mockinbird/parser';
 
 /**
  * The full test of MockGenerator can be found under the 'test' folder,
  * you can find there the full integration test
  */
-describe('MockGenerator', () => {
-  describe('given a MockGenerator', () => {
-    let generator: MockGenerator;
+describe('MockGenerator Unit Test', () => {
+  let generator: MockGenerator;
 
-    const parserMock = {
-      setFakerLocale: jest.fn(),
-      parse: jest.fn(),
-    } as unknown as ClassParser;
+  class TestClass {
+    test: string;
+  }
 
-    beforeAll(() => {
-      generator = new MockGenerator(parserMock);
-    });
+  const parserMock = {
+    setFakerLocale: jest.fn(),
+    parse: jest.fn(),
+  } as unknown as jest.Mocked<ClassParser>;
 
-    describe("when calling 'create' method", () => {
-      describe('with no options at all', () => {
-        beforeAll(() => generator.create(class TestClass {}));
+  beforeAll(() => {
+    generator = new MockGenerator(parserMock);
+    jest.clearAllMocks();
+  });
+
+  scenario('generate a SIMPLE mock from a class', () => {
+    given('I want to generate a new mock from a class', () => {
+      when('I call the create method with no configurations', () => {
+        beforeAll(() => generator.create(TestClass));
 
         test('then setup parser with the default locale', () => {
           expect(parserMock.setFakerLocale).toHaveBeenCalledWith('en');
         });
 
-        test('then use/call parse one time only', () => {
+        test('then call parse one time only', () => {
           expect(parserMock.parse).toHaveBeenCalledTimes(1);
         });
       });
+    });
+  });
 
-      describe('with an object argument containing the config/options (with all the properties)', () => {
-        class TestClass {}
+  scenario('generate mock from a class with a different configurations', () => {
+    given('I want to generate a mock with different locale', () => {
+      when('creating a new mock from generator passing the proper params', () => {
+        beforeAll(() => generator.create(TestClass, { locale: 'arbitrary-locale' }));
 
-        const options: MockGeneratorOptions = {
-          count: 3,
-          locale: 'arbitrary-locale',
-          override: { prop: 'value' },
-          ignore: ['test'],
-        };
-
-        beforeAll(() => {
-          jest.resetAllMocks();
-          generator.create(TestClass, options);
-        });
-
-        test('then setup the parser with the locale from the options', () => {
+        then('setup the parser with the locale from the options', () => {
           expect(parserMock.setFakerLocale).toHaveBeenCalledWith('arbitrary-locale');
         });
+      });
+    });
 
-        test('then call parse exactly 3 times', () => {
-          expect(parserMock.parse).toHaveBeenCalledTimes(3);
+    given('I want to generate a mock and override different values', () => {
+      when('creating a new mock from generator passing the proper param', () => {
+        beforeAll(() => generator.create(TestClass, { override: { test: 'value' } }));
+
+        then('call parse with the valid options', () => {
+          expect(parserMock.parse).toHaveBeenCalledWith(TestClass, { override: { test: 'value' } });
+        });
+      });
+    });
+
+    given('I want to generate a mock and ignore different values', () => {
+      when('creating a new mock from generator passing the proper param', () => {
+        beforeAll(() => generator.create(TestClass, { ignore: ['test'] }));
+
+        then('call parse with the valid options', () => {
+          expect(parserMock.parse).toHaveBeenCalledWith(TestClass, { ignore: ['test'] });
+        });
+      });
+    });
+
+    given('I want to generate a mock and convert it to plain', () => {
+      when('creating a new mock from generator passing the proper param', () => {
+        let result;
+
+        beforeAll(() => {
+          parserMock.parse.mockReturnValueOnce(
+            new (class Cat {
+              toy = new (class Toy {
+                bell = new (class Bell {})();
+              })();
+            })()
+          );
+
+          result = generator.create(TestClass, { plain: true });
         });
 
-        test('then call parse with the rest of the options', () => {
-          expect(parserMock.parse).toHaveBeenCalledWith(TestClass, { override: { prop: 'value' }, ignore: ['test'] });
+        then('', () => {
+          const isClass = (target) => target instanceof Object && target.constructor.name !== 'Object';
+
+          expect(isClass(result)).toBeFalsy();
+          expect(isClass(result.toy)).toBeFalsy();
+          expect(isClass(result.toy.bell)).toBeFalsy();
         });
       });
     });
