c9448eae72f7c6118d8ea4c1e6f108f533fc5931
omermorad <omer.moradd@gmail.com>
2021-08-13 09:51:26 +0300

    chore: change generator package to mockingbird package name

--- packages/mockingbird/src/lib/factory/mock-factory.ts
+++ packages/mockingbird/src/lib/factory/mock-factory.ts
@@ -1,8 +1,8 @@
 import { Faker, Class } from '@mockinbird/types';
 import { ClassParser } from '@mockinbird/parser';
 import { ClassReflector } from '@mockinbird/reflect';
-import { MockBuilder } from './builder';
-import { MockGenerator } from './generator/mock-generator';
+import { MockBuilder } from '../builder';
+import { MockGenerator } from '../generator/mock-generator';
 
 export function MockFactory<TClass>(target: Class<TClass>): MockBuilder<TClass> {
   const reflector = new ClassReflector();
