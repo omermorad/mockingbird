0984f450314fb6d9edd688be430f703daecacf34
omermorad <omer.moradd@gmail.com>
2021-08-26 23:57:01 +0300

    test(mockingbird-ts): check mock generation with regex

--- packages/mockingbird/test/integration/mock-generator.test.ts
+++ packages/mockingbird/test/integration/mock-generator.test.ts
@@ -8,6 +8,7 @@ import TestClassWithCallback = TestClasses.TestClassWithCallback;
 import TestClassWithEnum = TestClasses.TestClassWithEnum;
 import TestClassWithOtherClass = TestClasses.TestClassWithSingleClass;
 import TestClassWithMultiClass = TestClasses.TestClassWithMultiClass;
+import TestClassWithRegex = TestClasses.TestClassWithRegex;
 
 describe('MockGenerator - Integration Test', () => {
   let result;
@@ -15,7 +16,7 @@ describe('MockGenerator - Integration Test', () => {
   const generator = new MockGenerator(parser);
 
   describe('given a decorated class', () => {
-    describe('when using the mock decorator with absolute values', () => {
+    scenario('mock decorator with absolute values', () => {
       beforeAll(() => {
         result = generator.create(TestClassWithAbsoluteValues);
       });
@@ -26,7 +27,7 @@ describe('MockGenerator - Integration Test', () => {
         });
       });
 
-      describe('and adding some overrides properties', () => {
+      describe('and adding mutation keys', () => {
         beforeAll(() => {
           result = generator.create(TestClassWithAbsoluteValues, { mutations: { name: 'Override Name' } });
         });
@@ -36,7 +37,7 @@ describe('MockGenerator - Integration Test', () => {
         });
       });
 
-      describe('and adding some ignorance properties', () => {
+      describe('and adding omit keys', () => {
         beforeAll(() => {
           result = generator.create(TestClassWithAbsoluteValues, { omit: ['name'] });
         });
@@ -45,81 +46,93 @@ describe('MockGenerator - Integration Test', () => {
           expect(result.name).toBeUndefined();
         });
       });
+    });
 
-      describe('when using the mock decorator with a callback (faker)', () => {
-        beforeAll(() => {
-          result = generator.create(TestClassWithCallback);
-        });
-
-        test('then return random values from faker', () => {
-          expect(result).toMatchObject({
-            email: expect.any(String),
-            name: expect.any(String),
-          });
-        });
+    scenario('mock decorator with a callback (faker)', () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithCallback);
       });
 
-      describe('when using the mock decorator with an enum decoratorValue', () => {
-        beforeAll(() => {
-          result = generator.create(TestClassWithEnum);
-        });
-
-        test('then return one random decoratorValue (not key)', () => {
-          expect(['foo', 111, 'Bazz1234']).toContain(result.someEnumVal);
+      test('then return random values from faker', () => {
+        expect(result).toMatchObject({
+          email: expect.any(String),
+          name: expect.any(String),
         });
       });
+    });
 
-      describe('when using the mock decorator with no/empty values', () => {
-        beforeAll(() => {
-          result = generator.create(TestClassWithNoValues);
-        });
-
-        test('then infer the decoratorValue from the type itself', () => {
-          expect(result).toMatchSnapshot({
-            name: expect.any(String),
-            num: expect.any(Number),
-            binary: expect.any(Boolean),
-            date: expect.any(Date),
-          });
-        });
+    scenario('mock decorator with an enum decoratorValue', () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithEnum);
       });
 
-      describe('when using the mock decorator with a single class', () => {
-        beforeAll(() => {
-          result = generator.create(TestClassWithOtherClass);
-        });
+      test('then return one random decoratorValue (not key)', () => {
+        expect(['foo', 111, 'Bazz1234']).toContain(result.someEnumVal);
+      });
+    });
 
-        test('then return an object with the given class', () => {
-          expect(Object.keys(result.dog)).toEqual(['name', 'points']);
-        });
+    scenario('mock decorator with no/empty values', () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithNoValues);
       });
 
-      describe('when using the mock decorator with a multi class', () => {
-        beforeAll(() => {
-          result = generator.create(TestClassWithMultiClass);
-        });
-
-        test("then return contain a property 'dogs' which is array of Dog with length of 'count'", () => {
-          expect(result.dogs).toBeInstanceOf(Array);
-          expect(result.dogs).toHaveLength(3);
-        });
-
-        test('then return array of objects with the given class keys', () => {
-          expect(result.dogs).toEqual(
-            expect.arrayContaining([expect.objectContaining({ name: expect.any(String), points: expect.any(Number) })])
-          );
+      test('then infer the decoratorValue from the type itself', () => {
+        expect(result).toMatchSnapshot({
+          name: expect.any(String),
+          num: expect.any(Number),
+          binary: expect.any(Boolean),
+          date: expect.any(Date),
         });
       });
+    });
 
-      describe("when using the mock decorator with 'count' option", () => {
-        beforeAll(() => {
-          result = generator.create(TestClassWithAbsoluteValues, { count: 4, locale: 'ja' });
-        });
+    scenario('mock decorator with a single class', () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithOtherClass);
+      });
 
-        test("then return array with length of 'count'", () => {
-          expect(result).toBeInstanceOf(Array);
-          expect(result).toHaveLength(4);
-        });
+      test('then return an object with the given class', () => {
+        expect(Object.keys(result.dog)).toEqual(['name', 'points']);
+      });
+    });
+
+    describe('mock decorator with a regex', () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithRegex);
+      });
+
+      test('then return string generated to match the regex', () => {
+        expect(result.prop1).toBe('1234');
+        expect(result.prop2).toBe('regex');
+        expect(result.prop2).toMatch(/^[a-z]{4,5}$/);
+      });
+    });
+
+    scenario('mock decorator with a multi class', () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithMultiClass);
+      });
+
+      test("then return contain a property 'dogs' which is array of Dog with length of 'count'", () => {
+        expect(result.dogs).toBeInstanceOf(Array);
+        expect(result.dogs).toHaveLength(3);
+      });
+
+      test('then return array of objects with the given class keys', () => {
+        expect(result.dogs).toEqual(
+          expect.arrayContaining([expect.objectContaining({ name: expect.any(String), points: expect.any(Number) })])
+        );
+      });
+    });
+
+    scenario("mock decorator with 'count' option", () => {
+      beforeAll(() => {
+        result = generator.create(TestClassWithAbsoluteValues, { count: 4, locale: 'ja' });
+      });
+
+      test("then return array with length of 'count'", () => {
+        expect(result).toBeInstanceOf(Array);
+        expect(result).toHaveLength(4);
       });
     });
   });
