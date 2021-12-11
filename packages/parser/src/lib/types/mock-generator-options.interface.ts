dfdc1c9e765852aae9ae70558e51cdf66013407b
omermorad <omer.moradd@gmail.com>
2021-12-11 18:19:49 +0200

    refactor(parser): fix types and dependencies

--- packages/parser/src/lib/types/mock-generator-options.interface.ts
+++ packages/parser/src/lib/types/mock-generator-options.interface.ts
@@ -1,5 +1,5 @@
-import { ParserConfig } from '@mockingbird/parser';
 import { Faker } from '@mockingbird/common';
+import { ParserConfig } from './types';
 
 export interface MockGeneratorOptions<TClass = any> extends ParserConfig<TClass> {
   count?: number;
