81b0be799ef60ad03bab5da569e5caf42a608bf6
omermorad <omer.moradd@gmail.com>
2021-12-13 15:30:35 +0200

    refactor(mockingbird): remove redundant types, change exports
    BREAKING CHANGE:
    Remove export of `MockOptions`
    `Mock` decorator now takes different options
    Classes and enums in `Mock` decorator are now passed with lazy evaluation (through a function)

--- packages/mockingbird/src/index.ts
+++ packages/mockingbird/src/index.ts
@@ -1,7 +1,6 @@
+export { Mock, DecoratorValueArg } from '@mockingbird/reflect';
+
 export * from '@mockingbird/parser';
 export * from '@mockingbird/common';
 
-export { Mock, MockOptions } from '@mockingbird/reflect';
-
 export * from './lib';
-export * from './types/mock-generator-options.interface';
