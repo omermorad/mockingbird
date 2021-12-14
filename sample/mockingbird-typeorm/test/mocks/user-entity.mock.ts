a86d0afe3c99100e04fcb2eef9217171aa895da9
omermorad <omer.moradd@gmail.com>
2021-12-14 19:02:39 +0200

    revert: change build strategy remove preconstruct
    This reverts commit 6988b181

--- sample/mockingbird-typeorm/test/mocks/user-entity.mock.ts
+++ sample/mockingbird-typeorm/test/mocks/user-entity.mock.ts
@@ -1,35 +1,21 @@
 import { Mock } from 'mockingbird';
 import { UserEntity } from '../../src/entity/user.entity';
 
-export class ProductMock {
-  @Mock(({ datatype }) => datatype.uuid())
-  id!: string;
-
-  @Mock(({ commerce }) => commerce.product())
-  name!: string;
-}
-
 export class UserEntityMock implements UserEntity {
   id: number;
 
-  @Mock(({ name }) => name.firstName())
+  @Mock((faker) => faker.name.firstName())
   firstName!: string;
 
-  @Mock(({ name }) => name.lastName())
+  @Mock((faker) => faker.name.lastName())
   lastName!: string;
 
-  @Mock(({ address }) => address.streetAddress(true))
+  @Mock((faker) => faker.address.streetAddress(true))
   address!: string;
 
   @Mock()
   birthday!: Date;
 
-  @Mock(({ internet }) => internet.email())
+  @Mock((faker) => faker.internet.email())
   email!: string;
-
-  @Mock(() => ProductMock, { count: 3 })
-  products: ProductMock[];
-
-  @Mock(() => ProductMock)
-  product: ProductMock;
 }
